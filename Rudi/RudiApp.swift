d4e7399bb8064a310c564d5019c8f529988c2ac1
Kurt Höblinger <office@nitricware.com>
2020-09-26 08:02:26 +0200

    Initial Commit

---
+++ Rudi/RudiApp.swift
@@ -0,0 +1,20 @@
+//
+//  RudiApp.swift
+//  Rudi
+//
+//  Created by Kurt Höblinger on 26.09.20.
+//
+
+import SwiftUI
+
+@main
+struct RudiApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
